54e685947dea18d968d430b0c9d636a8d109e03a
madhukarwakhare <88505890+madhukarwakhare@users.noreply.github.com>
2024-04-29 10:45:39 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Madhukar Wakhare',
+    author_email= 'wakharemadhukar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
